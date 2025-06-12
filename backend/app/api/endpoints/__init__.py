27589b13556178f7fc2c75b052723af7de84b43d
Diana <merveille32@mail.ru>
2025-06-12 17:50:16 +0300

    [fix]:need to switch

--- backend/app/api/endpoints/__init__.py
+++ backend/app/api/endpoints/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from .family_tree import router as family_tree_router
+from .basic_checks import router as basic_checks_router
 
 router = APIRouter()
-router.include_router(family_tree_router, prefix="/family", tags=["family"]) 
+router.include_router(family_tree_router, prefix="/family", tags=["family"])
+router.include_router(basic_checks_router, prefix="/checks", tags=["checks"]) 
